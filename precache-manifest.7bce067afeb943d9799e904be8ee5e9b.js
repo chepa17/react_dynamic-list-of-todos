215cdb19d5389c46017584884cdbc1f1f93fb22a
Chepkyi Mykola <chepkiyn@gmail.com>
2019-11-06 17:25:26 +0200

    Updates

---
+++ precache-manifest.7bce067afeb943d9799e904be8ee5e9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f1fce8fc32068c92621a9ecc77e3f1",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "ac197acd0741f20e6f47",
+    "url": "/react_dynamic-list-of-todos/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "fc01d011af6823b55120",
+    "url": "/react_dynamic-list-of-todos/static/js/2.70e59209.chunk.js"
+  },
+  {
+    "revision": "ac197acd0741f20e6f47",
+    "url": "/react_dynamic-list-of-todos/static/js/main.f8d3f504.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
